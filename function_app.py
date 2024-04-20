a4dec7d7dd5180c4f5d5e3a76ce36bca874900d9
Pingo <kokatic@gmail.com>
2024-04-20 15:13:48 +0200

    Replace the endpoint with a fastapi app.

--- function_app.py
+++ function_app.py
@@ -1,28 +1,11 @@
 import azure.functions as func
-import datetime
-import json
-import logging
 
-app = func.FunctionApp()
+import fastapi
 
-@app.route(route="HttpExample", auth_level=func.AuthLevel.ANONYMOUS)
-def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+fastapi_app = fastapi.FastAPI()
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+@fastapi_app.get("/hello/{name}")
+async def get_name(name: str):
+    return f'Welcome {name}'
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
-
+app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
