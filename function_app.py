4bc9ca96cb5f31438dfe9879e3df2a344ff96f4d
Pingo <kokatic@gmail.com>
2024-04-20 21:04:12 +0200

    Add local settings, hello message and update .gitignore

--- function_app.py
+++ function_app.py
@@ -6,6 +6,6 @@ fastapi_app = fastapi.FastAPI()
 
 @fastapi_app.get("/hello/{name}")
 async def get_name(name: str):
-    return f'Welcome {name}'
+    return f'Hello {name}'
 
 app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
